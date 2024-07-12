da4107ad9962aeeecc42e98f6212665f278467c4
Devops Training <saravana.devopstraining2023@gmail.com>
2024-07-12 15:51:15 +0530

    spring mvc examples

---
+++ labs/examples/spring-mvc-examples/src/main/java/com/examples/spring/web/mvc/HomeController.java
@@ -0,0 +1,50 @@
+package com.examples.spring.web.mvc;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+ * Handles requests for the application home page.
+ */
+// @Component -> @Controller, @RestController, @Service, @Repository
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	// Path - / => http://localhost:8080/spring-mvc-examples/
+	// Method - GET, POST, PUT, DELETE
+	// Consumes
+	// Produces
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)  // Handler Mapping
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		// logical view name
+		return "home";
+	}
+	
+}
---
+++ labs/examples/spring-mvc-examples/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,13 @@
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Welcome to Spring MVC training! :)
+</h1>
+
+<P>  The time on the server is ${serverTime}. </P>
+</body>
+</html>
