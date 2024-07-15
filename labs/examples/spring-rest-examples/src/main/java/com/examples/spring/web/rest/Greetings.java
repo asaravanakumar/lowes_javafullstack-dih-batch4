9a61e7e257e8c110fdd2a86eef0102659765098a
Devops Training <saravana.devopstraining2023@gmail.com>
2024-07-15 15:58:54 +0530

    rest api examples

---
+++ labs/examples/spring-rest-examples/src/main/java/com/examples/spring/web/rest/Greetings.java
@@ -0,0 +1,20 @@
+package com.examples.spring.web.rest;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+//import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Greetings {
+	
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
