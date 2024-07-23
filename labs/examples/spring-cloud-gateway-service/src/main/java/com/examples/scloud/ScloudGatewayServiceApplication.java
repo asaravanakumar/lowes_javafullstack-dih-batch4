31855652da674a5480df360f627caaf64c30d229
Devops Training <saravana.devopstraining2023@gmail.com>
2024-07-23 16:16:51 +0530

    spring cloud gateway service example

---
+++ labs/examples/spring-cloud-gateway-service/src/main/java/com/examples/scloud/ScloudGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.examples.scloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScloudGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScloudGatewayServiceApplication.class, args);
+	}
+
+}
